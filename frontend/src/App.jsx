f756d51c0856bf038d59beeed7fbf194332c1e11
Sebastian <smunoz.id@gmail.com>
2025-10-09 15:51:47 -0300

    ScrollToTop

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,14 +5,16 @@ import AppRoutes from "./routes/AppRoutes";
 import Home from "./components/Home/Home";
 import About from "./components/About/About";
 import Events  from "./components/Events/Events";
+import ScrollToTop from "./components/ScrollToTop"; // Importa el componente
 
 const App = () => {
   return (
     <Router>
+      <ScrollToTop />
       <div className="flex flex-col min-h-screen">
         <NavBar
           links={[
-            { href: "/#areas", label: "Inicio" },
+            { href: "/", label: "Inicio" },
             { href: "/about", label: "Expertos" },
             { href: "/events", label: "Eventos y Cursos" },
             { href: "/#contacto", label: "Contacto" },
@@ -24,10 +26,9 @@ const App = () => {
             <Route path="/about" element={<About />} />
             <Route path="/events" element={<Events />} />
             <Route path="/*" element={<AppRoutes />} />
-            {/* Rutas principales */}
           </Routes>
         </main>
-        <Footer /> {/* Footer siempre visible */}
+        <Footer />
       </div>
     </Router>
   );
